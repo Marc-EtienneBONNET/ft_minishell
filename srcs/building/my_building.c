923519fd50dd78acb04887921f0048d7995c5198
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-01-06 16:37:13 +0100

    pwd echo

--- srcs/building/my_building.c
+++ srcs/building/my_building.c
@@ -12,3 +12,56 @@
 
 #include "minishell.h"
 
+int		option(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_strlen(str) >= 2)
+	{
+		if (str[0] == '-' && str[1] == 'n')
+		{
+			while (str[++i])
+			{
+				if (str[i] != 'n')
+					return (0);
+				return (1);
+			}
+		}
+	}
+	return (0);
+}
+
+int		ft_echo(char **argv)
+{
+	int	i;
+	int	flag;
+
+	i = 1;
+	flag = 0;
+	while (argv[i] && option(argv[i]))
+	{
+		flag = 1;
+		i++;
+	}
+	if (!(argv[i]) && flag == 0)
+		printf("\n");
+	while (argv[i])
+	{
+		printf("%s", argv[i]);
+		if (argv[i + 1])
+			printf(" ");
+		else if (!(argv[i + 1]) && flag == 0)
+			printf("\n");
+		i++;
+	}
+	return (0);
+}
+
+int		pwd(void)
+{
+	char *pwd;
+	pwd = getcwd(NULL, 0);
+	printf("%s\n", pwd);
+	return (0);
+}
