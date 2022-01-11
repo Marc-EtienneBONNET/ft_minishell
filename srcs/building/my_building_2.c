14633bf8e205bee5ea4756c880a3b74992eecbb3
Marc-Etienne Bonnet <webbonnet@gmail.com>
2022-01-11 13:00:17 +0100

    ajoue de cd

---
+++ srcs/building/my_building_2.c
@@ -0,0 +1,81 @@
+#include "minishell.h"
+
+int		change_pwd(char *pwd, char *oldpwd)
+{
+	int i;
+
+	i = -1;
+	while (term->my_env[++i].key)
+	{
+		if (ft_strncmp(term->my_env[i].key, "PWD", 3) == 0)
+		{
+			free(term->my_env[i].var);
+			term->my_env[i].var = ft_strdup(pwd);
+			free(pwd);
+		}
+	}
+	i = -1;
+	while (term->my_env[++i].key)
+	{
+		if (ft_strncmp(term->my_env[i].key, "OLDPWD", 6) == 0)
+		{
+			free(term->my_env[i].var);
+			term->my_env[i].var = ft_strdup(oldpwd);
+			free(oldpwd);
+		}
+	}
+	return (0);
+}
+
+int		op(char **argv)
+{
+	int i;
+
+	i = -1;
+	if (ft_strncmp(argv[1], "-", 1) == 0)
+	{
+		while (term->my_env[++i].key)
+		{
+			if (ft_strncmp(term->my_env[i].key, "OLDPWD", 6) == 0 && term->my_env[i].var)
+			{
+				chdir(term->my_env[i].var);
+				printf("%s\n", term->my_env[i].var);
+				return (0);
+			}
+		}
+	}
+	if (chdir(argv[1]) == -1)
+	{
+		write(2, "bash: cd: ", 10);
+		write(2, argv[1], ft_strlen(argv[1]));
+		write(2, ": ", 2);
+		perror("");
+	}
+	return (0);
+}
+
+int my_cd(char **argv)
+{
+	char	*pwd;
+	char	*oldpwd;
+	int		i;
+
+	i = -1;
+	oldpwd = getcwd(NULL, 0);
+	if (!argv[1] || (ft_strncmp(argv[1], "--", 2) == 0) || (ft_strncmp(argv[1], "~", 1) == 0))
+	{
+		while (term->my_env[++i].key)
+		{
+			if (ft_strncmp(term->my_env[i].key, "HOME", 4) == 0)
+			{
+				pwd = getcwd(NULL, 0);
+				chdir(term->my_env[i].var);
+				return (change_pwd(pwd, oldpwd));
+			}
+		}
+	}
+	op(argv);
+	pwd = getcwd(NULL, 0);
+	change_pwd(pwd, oldpwd);
+	return (0);
+}
